b6b188e942b83ee77379f2c02015009c03c894f9
Lori Henderson <lorim95@gmail.com>
2023-05-23 10:25:46 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  username='';
+
+  onUpdateUsername(event: Event) {
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+}
